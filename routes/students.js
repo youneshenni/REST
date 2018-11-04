83a6967c399ec290edc6a8fe6516f9c4d6f31192
younabobo <younabobo3@gmail.com>
2018-11-04 18:19:22 +0100

    Update and Delete are ready, But there's a problem with the database

--- routes/students.js
+++ routes/students.js
@@ -3,14 +3,22 @@ const Student = require('../models/Student.js')
 
 module.exports = server => {
 
+    server.get('/students/', (req, res, next) => {
+        Student.find(req.params, (err, documents) => {
+            if (err) return console.error(err)
+            res.send(documents)
+        })
+        next()
+    })
+
     server.get('/students/:firstName', (req, res, next) => {
         Student.find(req.params, (err, documents) => {
             if (err) return console.error(err)
             res.send(documents)
-            next(new errors.InvalidContentError(err))
         })
         next()
     })
+
     server.post('/students', (req, res, next) => {
         //Check if it's in JSON format
         if (!req.is('application/json')) {
@@ -20,4 +28,29 @@ module.exports = server => {
         newStudent.save(err => {if (err) return console.error(err)})
         res.send(201)
     })
+    //PUT   DELETE
+    server.put('/students/:id', (req, res, next) => {
+        if (!req.is('application/json')) {
+            return next(new errors.InvalidContentError('application/json format expected'))
+        }
+        try {
+            var student = Student.findOneAndUpdate({ _id: req.params.id}, req.body)
+            student.save()
+            res.send(200)
+            next()
+        } catch (err) {
+            return next(new errors.ResourceNotFoundError(`There is no Student with ${req.params.id} as ID`))
+        }
+    })
+
+    server.del('/students/:_id', (req, res, next) => {
+        try {
+            var student = Student.findOneAndRemove(req.params)
+            res.send(204)
+            next()
+        } catch (err) {
+            return next(new errors.ResourceNotFoundError(`There is no Student with ${req.params.id} as ID`))
+        }
+
+        })
 }
